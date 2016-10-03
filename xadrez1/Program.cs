d627b4ae1109ec57288a7bdf83e9bf804e46efcc
Igor Altieres <igoraltieres@gmail.com>
2016-10-03 09:00:20 -0300

    Projeto criado,

---
+++ xadrez1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+        
+            Console.ReadLine();
+            
+        }
+    }
+}
