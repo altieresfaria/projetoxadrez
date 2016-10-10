bd27e2bfcb7b5d9747b4432d9f8f8e3fe7376b57
Igor Altieres <igoraltieres@gmail.com>
2016-10-10 12:02:46 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez1/Program.cs
+++ xadrez1/Program.cs
@@ -9,6 +9,26 @@ namespace xadrez1
         {
             try {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada) {
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                
+
+
+
+
+                    partida.executaMovimento(origem, destino);
+
+                }
                 
 
              
