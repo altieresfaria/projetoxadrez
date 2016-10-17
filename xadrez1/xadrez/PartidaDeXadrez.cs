740dc842000951cec168d0d827515fc29c1aedf6
IGOR ALTIERES FARIA SILVA <13591249696@est.udicentro.local>
2016-10-17 11:10:47 -0200

    Implementando as restricoes de movimento

--- xadrez1/xadrez/PartidaDeXadrez.cs
+++ xadrez1/xadrez/PartidaDeXadrez.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using tabuleiro;
 
+
 namespace xadrez {
     class PartidaDeXadrez {
 
-        public Tabuleiro tab { get;  private set; }
-        private int turno;
-        private Cor JogadorAtual;
+        public Tabuleiro tab { get; private set; }
+        public int turno { get; private set; }
+        public Cor JogadorAtual { get; private set; }
         public bool terminada { get; private set; }
 
         public PartidaDeXadrez() {
@@ -22,6 +23,43 @@ namespace xadrez {
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
         }
+
+        public void realizaJogada(Posicao origem, Posicao destino) {
+            executaMovimento(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+
+        public void validarPosicaodeOrigem(Posicao pos) {
+            if (tab.peca(pos) == null) {
+                throw new TabuleiroException("Não existe peça na posicao de origem escolhida!");
+            }
+            if (JogadorAtual != tab.peca(pos).cor) {
+                throw new TabuleiroException("A peça de origem escolhida não é sua!");
+            }
+            if (!tab.peca(pos).existeMovimentosPossiveis()) {
+                throw new TabuleiroException("Não há movimentos possíveis para a peça de origem  escolhida!"); }
+        }
+    
+
+    public void validarPosicaoDeDestino(Posicao origem, Posicao destino) {
+        if (!tab.peca(origem).podeMoverPara(destino)) {
+            throw new TabuleiroException("Posição de destino inválida!");
+
+
+        }
+    }
+
+        private void mudaJogador() {
+            if(JogadorAtual == Cor.Branca) {
+                JogadorAtual = Cor.Preta;
+            }
+            else {
+                JogadorAtual = Cor.Branca;
+            }
+
+        }
+
         private void colocarPecas() {
             tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c',1). toPosicao());
             tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c', 2).toPosicao());
